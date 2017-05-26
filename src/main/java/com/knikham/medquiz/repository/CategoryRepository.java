18ea7572f7569338f221d9058ced8b8e737f64f8
kniaz <kniaz.dm@gmail.com>
2017-05-26 21:03:53 +0300

    Created CategoryRepository class.

--- src/main/java/com/knikham/medquiz/repository/CategoryRepository.java
+++ src/main/java/com/knikham/medquiz/repository/CategoryRepository.java
@@ -1,8 +1,8 @@
 package com.knikham.medquiz.repository;
 
-
 import com.knikham.medquiz.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByTitle(String title);
