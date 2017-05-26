fa777a95a55ef0ff4c006db159adaab811a002f3
kniaz <kniaz.dm@gmail.com>
2017-05-26 19:03:07 +0300

    Created Spring Boot application class.

---
+++ src/main/java/com/knikham/medquiz/MedQuizApplicatoin.java
@@ -0,0 +1,12 @@
+package com.knikham.medquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MedQuizApplicatoin {
+    public static void main(String[] args) {
+        SpringApplication.run(MedQuizApplicatoin.class, args);
+    }
+}
