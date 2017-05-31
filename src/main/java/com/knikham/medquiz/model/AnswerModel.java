cd55cefee7321385321509581aea70f4054d5ec8
kniaz <kniaz.dm@gmail.com>
2017-05-31 17:38:22 +0300

    Created AnswerModel class.

---
+++ src/main/java/com/knikham/medquiz/model/AnswerModel.java
@@ -0,0 +1,15 @@
+package com.knikham.medquiz.model;
+
+
+public class AnswerModel {
+    private String body;
+    private boolean isRight;
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
